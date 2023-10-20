ed6f1b56d8746e957092f01dcfd1bdec02c3012f
Eustaquio Ferreira <eusferalves@gmail.com>
2023-10-20 18:07:48 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+
+import {DiceRoll} from '@dice-roller/rpg-dice-roller';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'malditos-goblins';
+
+  constructor(){
+
+  }
+
+  public goblins: any[] = [];
+
+
+  newGoblin(){
+    
+
+    const roll = new DiceRoll('1d6');
+
+    console.log(roll.total);
+    // roll.roll()
+    console.log(roll.total);
+    this.goblins.push({name: 'teste'})
+  }
 }
